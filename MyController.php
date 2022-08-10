49eb9af5af6c405f1f77dcc1fecc92ea2e59247e
Web Full Stack Developer <88669256+yusufjonc07@users.noreply.github.com>
2022-08-10 20:46:56 +0500

    Create MyController.php

---
+++ MyController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class TablesController extends Controller
+{
+  public function getAllControllerActions()
+    {
+        $controllers = \yii\helpers\FileHelper::findFiles(Yii::getAlias('@app/controllers'), ['recursive' => true]);
+        $actions = [];
+        foreach ($controllers as $controller) {
+            $contents = file_get_contents($controller);
+            $controllerId = Inflector::camel2id(substr(basename($controller), 0, -14));
+            preg_match_all('/public function action(\w+?)\(/', $contents, $result);
+            foreach ($result[1] as $action) {
+                $actionId = Inflector::camel2id($action);
+                $route = $controllerId . '/' . $actionId;
+                $actions[$route] = $route;
+            }
+        }
+        asort($actions);
+        return $actions;
+    }
